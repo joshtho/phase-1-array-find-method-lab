a0196e04228361a93ae54fed9bdf2655e9ccd34a
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-03-09 16:45:29 -0600

    commit tree c5e9f11199814f5812287473b1bbecb5ba04a8ed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+// function superbowlWin(record) {
+//     let item = record.find(obj => obj.result === "W")
+//     if (item) {
+//         return item.year
+//     } 
+function superbowlWin(record) {
+    if (record.find(record.result === 'W')) {
+        return record.year
+    };
+}
+
